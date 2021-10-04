83ea71585d98bb872db247c4ed3026c44e4041c7
scorztv <sorzech@me.com>
2021-10-04 23:30:49 +0800

    Exercise 3.8 - log data sent in POST requests to console

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,7 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+morgan.token('data', function (request, response) {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+})
 
 let persons = [
   { 
