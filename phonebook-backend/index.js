70fb9de4d5a22ff44ad28d43c126f344383595d7
scorztv <sorzech@me.com>
2021-10-06 15:17:30 +0800

    Exercise 3.11 - adding production build of frontend to deploy :O

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,6 +13,7 @@ morgan.token('data', function (request, response) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   { 
