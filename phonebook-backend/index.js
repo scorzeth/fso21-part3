ad44d403207acb70ceff42fb867bf9e335310be8
scorztv <sorzech@me.com>
2021-10-10 19:46:46 +0800

    Exercise 3.19 - added a validator, POST cant receive duplicate names

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -53,7 +53,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -105,6 +105,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
