ceae3d21916d83923af94b795eb95944f43023e9
scorztv <sorzech@me.com>
2021-10-10 17:36:13 +0800

    Exercise 3.12 - adding a database on mongodb

---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.sqhtj.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length >= 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+      name: name,
+      number: number
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
